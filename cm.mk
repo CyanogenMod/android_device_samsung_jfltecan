c049de927c4f24c590e986f3f53d6ad3658adf13
Kasual52e <mushroomhead52e@gmail.com>
2015-01-25 00:40:38 -0800

    jfltecan: initial cm12 commit

--- cm.mk
+++ cm.mk
@@ -6,8 +6,11 @@ $(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltevl TARGET_DEVICE=jfltecan BUILD_FINGERPRINT="samsung/jfltevl/jfltecan:4.2.2/JDQ39/I337MVLUAMDJ:user/release-keys" PRIVATE_BUILD_DESC="jfltevl-user 4.2.2 JDQ39 I337MVLUAMDJ release-keys"
+PRODUCT_BUILD_PROP_OVERRIDES += \
+  PRODUCT_NAME=jfltevl \
+  TARGET_DEVICE=jfltecan \
+  BUILD_FINGERPRINT="samsung/jfltevl/jfltecan:4.4.2/KOT49H/I337MVLUFNC1:user/release-keys" \
+  PRIVATE_BUILD_DESC="jfltevl-user 4.4.2 KOT49H I337MVLUFNC1 release-keys"
 
 PRODUCT_NAME := cm_jfltecan
 PRODUCT_DEVICE := jfltecan
-
